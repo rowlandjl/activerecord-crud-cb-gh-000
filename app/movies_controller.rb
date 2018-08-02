c694ac64e863794f82f0a64fbe6eaff1035fa9d1
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-02 17:12:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,13 +6,13 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.create
+  movie = Movie.create 
   movie.title = "This is a title."
-  movie.save
+  __
 end
 
 def can_be_created_with_a_hash_of_attributes
-
+  # Initialize movie and then and save it
   attributes = {
       title: "The Sting",
       release_date: 1973,
@@ -20,14 +20,14 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.create(attributes)
+  movie = __
 end
 
 def can_be_created_in_a_block(args = __)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-
+  
   Movie.create do |m|
     __
   end
@@ -58,7 +58,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by
+  # For this test return all movies released after 2002 and ordered by 
   # release date descending
   __
 end
